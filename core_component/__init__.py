9d2249e13ce50625b67551f32684659ee07a173d
James Barwick <jbarwick@me.com>
2025-09-25 23:54:09 +0800

    format

--- core_component/__init__.py
+++ core_component/__init__.py
@@ -3,7 +3,7 @@ from .preprocessor import load_user_variables, render_component_defintitions
 from .validator import validate_component, validate_specs
 from .handler import handler as pipeline_compiler
 
-__version__ = "0.0.11-pre.3+fb027db"
+__version__ = "0.1.2-pre.2+7bd6f58"
 
 __all__ = [
     "pipeline_compiler",
